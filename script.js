4eabcb82d00282ae62c54a35e792eb63f579770f
Jael Masllorens <jmasllorens@gmail.com>
2021-04-26 17:54:37 +0200

    Segunda elección: 'Papeles y cenizas' / en comentarios

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 alert("Aquesta és una pàgina interactiva (on i offline). Tria un títol, fes-me'l saber i s'habilitarà la lectura de l'article corresponent.")
 let mainPrincipal  = document.querySelector(".main-principal")
 let mainLibro1 = document.querySelector(".main-libro1")
-
+let mainLibro4 = document.querySelector(".main-libro4")
 
 function cambiarPantallaLibro1(){
 
@@ -12,33 +12,16 @@ function cambiarPantallaLibro1(){
         mainLibro1.style.display = "block"
         alert("Context: (1) Buenos Aires, 2016. (2) Primeres línies després d'uns 8 anys de bloqueig sense poder escriure.")
         }
-    
-
 }
 
-function cambiarPantallaPrincipal(){
+function cambiarPantallaLibro4(){
 
-    if(mainCatalogo.style.display == "block"){
-        mainCatalogo.style.display = "none"
+    if(mainPrincipal.style.display == "block"){
+        mainPrincipal.style.display = "none"
         }
-    if(mainPrincipal.style.display == "none"){
-        mainPrincipal.style.display = "block"
+    if(mainLibro4.style.display == "none"){
+        mainLibro4.style.display = "block"
+        alert("Context: (1) Barcelona, 2019. (2) Primeres línies després de l'última visita a Argentina.")
         }
-    if(mainDescripcion.style.display == "block"){
-        mainDescripcion.style.display = "none"
-        }
-
-}
-
-function cambiarPantallaDescripcion(){
-
-    if(mainCatalogo.style.display == "block"){
-        mainCatalogo.style.display = "none"
-        }
-    if(mainDescripcion.style.display == "none"){
-        mainDescripcion.style.display = "block"
-        }
-    
-
 }
 
