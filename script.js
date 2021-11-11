8f54364ab166c8efc54ffc2aef66306f27bcd6c9
Jael Masllorens <jmasllorens@gmail.com>
2021-11-11 10:26:54 +0100

    Rest of the texts added

--- script.js
+++ script.js
@@ -1,8 +1,63 @@
-alert("Aquesta és una pàgina interactiva (on i offline). Tria un títol, fes-me'l saber i s'habilitarà la lectura de l'article corresponent.")
+
 let mainPrincipal  = document.querySelector(".main-principal")
 let mainLibro1 = document.querySelector(".main-libro1")
 let mainLibro4 = document.querySelector(".main-libro4")
 let mainLibro2 = document.querySelector(".main-libro2")
+let mainLibro3 = document.querySelector(".main-libro3")
+let mainLibro5 = document.querySelector(".main-libro5")
+let mainLibro6 = document.querySelector(".main-libro6")
+let mainLibro7 = document.querySelector(".main-libro7")
+let mainLibro8 = document.querySelector(".main-libro8")
+let mainLibro9 = document.querySelector(".main-libro9")
+let mainLibro10 = document.querySelector(".main-libro10")
+
+
+function volverIndex() {
+
+    if(mainLibro1.style.display == "block"){
+        mainLibro1.style.display = "none"
+        }
+
+    if(mainLibro2.style.display == "block"){
+        mainLibro2.style.display = "none"
+        }
+
+    if(mainLibro3.style.display == "block"){
+        mainLibro3.style.display = "none"
+        }
+   
+
+    if(mainLibro4.style.display == "block"){
+        mainLibro4.style.display = "none"
+        }
+
+    if(mainLibro5.style.display == "block"){
+        mainLibro5.style.display = "none"
+        }
+    
+    if(mainLibro6.style.display == "block"){
+        mainLibro6.style.display = "none"
+        }
+    
+    if(mainLibro7.style.display == "block"){
+        mainLibro7.style.display = "none"
+        }
+
+    if(mainLibro8.style.display == "block"){
+        mainLibro8.style.display = "none"
+        }
+    
+    if(mainLibro9.style.display == "block"){
+        mainLibro9.style.display = "none"
+        }
+    
+    if(mainLibro10.style.display == "block"){
+        mainLibro10.style.display = "none"
+        }
+
+    if(mainPrincipal.style.display = "none") {
+    mainPrincipal.style.display = "block"}
+}
 
 function cambiarPantallaLibro1(){
 
@@ -11,7 +66,6 @@ function cambiarPantallaLibro1(){
         }
     if(mainLibro1.style.display == "none"){
         mainLibro1.style.display = "block"
-        alert("Context: (1) Buenos Aires, 2016. (2) Primeres línies després d'uns 8 anys de bloqueig sense poder escriure.")
         }
 }
 
@@ -22,7 +76,6 @@ function cambiarPantallaLibro4(){
         }
     if(mainLibro4.style.display == "none"){
         mainLibro4.style.display = "block"
-        alert("Context: (1) Barcelona, 2019. (2) Primeres línies després de l'última visita a Argentina.")
         }
 }
 
@@ -33,7 +86,81 @@ function cambiarPantallaLibro2(){
         }
     if(mainLibro2.style.display == "none"){
         mainLibro2.style.display = "block"
-        alert("Context: Barcelona, 2017.")
         }
 }
+
+function cambiarPantallaLibro3(){
+
+    if(mainPrincipal.style.display == "block"){
+        mainPrincipal.style.display = "none"
+        }
+    if(mainLibro3.style.display == "none"){
+        mainLibro3.style.display = "block"
+        }
+}
+
+function cambiarPantallaLibro5(){
+
+    if(mainPrincipal.style.display == "block"){
+        mainPrincipal.style.display = "none"
+        }
+    if(mainLibro5.style.display == "none"){
+        mainLibro5.style.display = "block"
+        }
+}
+
+function cambiarPantallaLibro6(){
+
+    if(mainPrincipal.style.display == "block"){
+        mainPrincipal.style.display = "none"
+        }
+    if(mainLibro6.style.display == "none"){
+        mainLibro6.style.display = "block"
+        }
+}
+
+function cambiarPantallaLibro7(){
+
+    if(mainPrincipal.style.display == "block"){
+        mainPrincipal.style.display = "none"
+        }
+    if(mainLibro7.style.display == "none"){
+        mainLibro7.style.display = "block"
+        }
+}
+
+function cambiarPantallaLibro8(){
+
+    if(mainPrincipal.style.display == "block"){
+        mainPrincipal.style.display = "none"
+        }
+    if(mainLibro8.style.display == "none"){
+        mainLibro8.style.display = "block"
+        }
+}
+
+function cambiarPantallaLibro9(){
+
+    if(mainPrincipal.style.display == "block"){
+        mainPrincipal.style.display = "none"
+        }
+    if(mainLibro9.style.display == "none"){
+        mainLibro9.style.display = "block"
+        }
+}
+
+function cambiarPantallaLibro10(){
+
+    if(mainPrincipal.style.display == "block"){
+        mainPrincipal.style.display = "none"
+        }
+    if(mainLibro10.style.display == "none"){
+        mainLibro10.style.display = "block"
+        }
+}
+
+
+
+
+
 
